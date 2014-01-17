914104b686abecd7e52859ee43389bfaf0c0464e
Pawel Suder <pawel@suder.info>
2014-01-17 12:25:06 +0100

    Fix deb creation

--- pkg/deb-pkg/create-deb.sh
+++ pkg/deb-pkg/create-deb.sh
@@ -8,18 +8,19 @@ Version: 0.1
 Maintainer: Pawel Suder <pawel@suder.info>
 Depends: davfs2
 Architecture: all
-Files: lobcderd.conf /etc/default
-    lobcderd.sh /etc/init.d
+Files: lobcderd.conf /etc/default/lobcderd
+    lobcderd /etc/init.d
+
 File: postinst
     #!/bin/sh
-    /usr/sbin/update-rc.d lobcderd.sh defaults
+    update-rc.d lobcderd defaults
 
 File: postrm
     #!/bin/sh
-    /usr/sbin/update-rc.d -f lobcderd.sh remove
+    update-rc.d -f lobcderd remove
 
-Description: Manage LOBCDER VPH-Share resource
- Manage LOBCDER VPH-Share resource." | cat > lobcderd
+Description: Automount LOBCDER VPH-Share resource
+ Provide automount scripts to mount LOBCDER resource via davfs." | cat > lobcderd.spec
 
-equivs-build lobcderd
-lintian *.deb
+equivs-build lobcderd.spec
+lintian lobcderd*.deb
