715a1246c2813f1b8559859903e056f559d6aed0
Pawel Suder <pawel@suder.info>
2014-01-21 19:41:20 +0100

    Arch pkg created

--- pkg/pkg-pkg/create-pkg.sh
+++ pkg/pkg-pkg/create-pkg.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 mkdir ./pkg
-cp PKGBUILD ./pkg/.PKGINFO
+cp PKGINFO ./pkg/.PKGINFO
+cp INSTALL ./pkg/.INSTALL
 cp -r etc/ ./pkg
+cp -r usr/ ./pkg
 cd pkg/
-tar -cf - .PKGINFO * | xz -c -z - > ../lobcderd.pkg.tar.xz
+tar -cf - .PKGINFO .INSTALL * | xz -c -z - > ../lobcderd.pkg.tar.xz
 cd ../
 rm -rf pkg
