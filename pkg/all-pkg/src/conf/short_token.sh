101435f5cfc463c5538aaff1f47f1009aeb6c8af
Pawel Suder <pawel@suder.info>
2014-02-01 00:25:06 +0100

    Fix for curl. Using insecure option.

--- pkg/all-pkg/src/conf/short_token.sh
+++ pkg/all-pkg/src/conf/short_token.sh
@@ -6,5 +6,5 @@ export LOBCDER_SHORT_TOKEN_URL=https://lobcder.vph.cyfronet.pl/lobcder/urest/get
 
 short_token()
 {
-    echo `curl ${LOBCDER_SHORT_TOKEN_URL}/$1 2>/dev/null`
+    echo `curl --insecure ${LOBCDER_SHORT_TOKEN_URL}/$1 2>/dev/null`
 }
--- pkg/pkg-pkg/etc/lobcder/get_ticket.sh
+++ pkg/pkg-pkg/etc/lobcder/get_ticket.sh
@@ -6,5 +6,5 @@ export LOBCDER_USER_DATA_URL=http://169.254.169.254/openstack/latest/user_data
 
 get_ticket()
 {
-    echo `curl ${LOBCDER_USER_DATA_URL} 2>/dev/null`
+    echo `curl --insecure ${LOBCDER_USER_DATA_URL} 2>/dev/null`
 }
